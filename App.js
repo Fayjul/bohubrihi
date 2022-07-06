226d2f83874cbd69f95344dd8fee9c23c7995064
Fayjul Islam <fayzulislam9@gmail.com>
2022-07-06 16:49:13 +0600

    nothing

---
+++ App.js
@@ -0,0 +1,9 @@
+const fun = () => {
+  console.log('Bangladesh');
+};
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(2, 'ban'));
+fun();
+console.log(module);
---
+++ circle.js
@@ -0,0 +1,8 @@
+const Area = (r) => 3.13 * r * r;
+const circumferance = (r) => 3.14 * 2 * r;
+const message = 'Allah Please help me';
+
+exports.area = Area;
+exports.circumferance = circumferance;
+exports.msg = message;
+console.log(module);
