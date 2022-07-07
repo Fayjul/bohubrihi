fa1368888df7e31dbf34f0376416a39e95722c47
Fayjul Islam <fayzulislam9@gmail.com>
2022-07-07 15:45:47 +0600

    synchronus and asynchronus module

--- App.js
+++ App.js
@@ -1,3 +1,29 @@
+const fs = require('fs');
+
+fs.readFile('./files/input.txt', 'utf-8', (error, text) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(text);
+    fs.writeFile('./files/output2.txt', text, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
+
+/*
+sychronus system
+let text = fs.readFileSync('./files/input.txt', 'utf-8');
+text = `Copide text: ${text}`;
+fs.writeFileSync('./files/output.txt', text);
+*/
+//console.log(text);
+// Getting start with circle.js
+/*
+const circle = require('./circle');
+
 const fun = () => {
   console.log('Bangladesh');
 };
@@ -7,3 +33,7 @@ const add = (a, b) => {
 console.log(add(2, 'ban'));
 fun();
 console.log(module);
+*/
+//console.log(circle(3));
+//console.log(circle.msg);
+//console.log(circle.circumferance(3));
--- circle.js
+++ circle.js
@@ -1,8 +1,12 @@
+// Getting started
+/*
 const Area = (r) => 3.13 * r * r;
 const circumferance = (r) => 3.14 * 2 * r;
 const message = 'Allah Please help me';
 
-exports.area = Area;
-exports.circumferance = circumferance;
-exports.msg = message;
-console.log(module);
+module.exports = Area;
+//exports.circumferance = circumferance;
+//exports.msg = message;
+//console.log(message);
+//console.log(module);
+*/
